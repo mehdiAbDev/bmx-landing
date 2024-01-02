c56e40236468cb2718c9e2965735a983dd50f18c
mehdi <abbanemehdi2006@gmail.com>
2024-01-02 14:59:45 +0100

    Please enter the commit message for your changes. Lines starting

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const viewBmx = document.getElementById('viewbmx')
+const bikes = document.querySelectorAll('.bike')
+bikes.forEach(elem => elem.style.backgroundImage = `linear-gradient(to left ,${elem.dataset.color}, rgba(0,0,0, .1))`)
+const svgElement = document.getElementById('SvgjsSvg1106')
+const bikesContainer = document.querySelector('.bikes')
+bikesContainer.addEventListener('click', function(event) {
+    const item = event.target.closest('.bike')
+    if(!item) return
+    viewBmx.src= item.dataset.src
+    svgElement.querySelector('path').setAttribute('fill', item.dataset.color)
+    viewBmx.style.transform = `rotate(-${item.dataset.rotate}deg)`
+
+})
