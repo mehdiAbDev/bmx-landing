813a152c53ac88ceddb42ffd241469a17e2016b6
mehdi <abbanemehdi2006@gmail.com>
2024-01-15 09:52:24 +0100

    saf

--- src/script.js
+++ src/script.js
@@ -1,13 +1,21 @@
 const viewBmx = document.getElementById('viewbmx')
 const bikes = document.querySelectorAll('.bike')
-bikes.forEach(elem => elem.style.backgroundImage = `linear-gradient(to left ,${elem.dataset.color}, rgba(0,0,0, .1))`)
 const svgElement = document.getElementById('SvgjsSvg1106')
 const bikesContainer = document.querySelector('.bikes')
+document.addEventListener('load', function() {
+
+})
 bikesContainer.addEventListener('click', function(event) {
     const item = event.target.closest('.bike')
     if(!item) return
-    viewBmx.src= item.dataset.src
+    const highQualityImageSrc = `${item.dataset.src}high.png`
+    const tempImage = new Image()
+    tempImage.src = highQualityImageSrc
+    tempImage.onload = function() {
+        viewBmx.src = highQualityImageSrc;
+        console.log('Loaded succ');
+    }
+    console.log(highQualityImageSrc);
     svgElement.querySelector('path').setAttribute('fill', item.dataset.color)
     viewBmx.style.rotate = `-${item.dataset.rotate}deg`
-
 })
