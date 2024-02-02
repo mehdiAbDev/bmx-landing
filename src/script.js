7deabd64d681d9f99e7fd19986d00d48266ec209
mehdi <abbanemehdi2006@gmail.com>
2024-02-02 17:50:57 +0100

    hello world

--- src/script.js
+++ src/script.js
@@ -9,6 +9,7 @@ bikesContainer.addEventListener('click', function(event) {
     const item = event.target.closest('.bike')
     if(!item) return
     const highQualityImageSrc = `${item.dataset.src}high.png`
+    console.log(highQualityImageSrc);
     const tempImage = new Image()
     tempImage.src = highQualityImageSrc
     tempImage.onload = function() {
